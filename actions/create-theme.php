fb6a0e301b1243e8419c993e6b8876d8743db34d
Suleyman <youmsv@gmail.com>
2017-11-16 17:02:42 +0300

    Eight

---
+++ actions/create-theme.php
@@ -0,0 +1,12 @@
+<?php
+
+$link = mysqli_connect("localhost", "root", "0202", "base");
+if( $_POST['btn1'] ) {
+	$head = $_POST['head']; $text = $_POST['text'];
+	if( $head && $text ) {
+		mysqli_query($link, "INSERT INTO themes VALUES( NULL, NOW(), '$head', '$text' )");
+		header('Location: ../pages/forum.php');
+	}
+}
+
+?>
--- blocks/create-form.php
+++ blocks/create-form.php
@@ -1,1 +1,7 @@
-gfgfgfg
+<form action="../actions/create-theme.php" method="POST">
+	<b>Введите заголовок темы</b><br>
+	<textarea name="head" id="" cols="80" rows="2" minlength="10" maxlength="150" style="resize:none;"></textarea><br>
+	<b>Введите текст</b><br>
+	<textarea name="text" id="" cols="80" rows="12" minlength="50" maxlength="500" style="resize:none;"></textarea><br>
+	<button name="btn1"><b>Создать</b></button>
+</form>
